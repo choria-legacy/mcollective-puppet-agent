a8dbbac333377590be177bb6e90ec28dbc9febd1
Richard Clamp <richardc@unixbeard.net>
2014-05-16 16:39:52 +0100

    maint - ensure we load puppet_server_address_validation
    mcollectived when processing the DDL doesn't actually load all the
    validators, so we can't rely on the transitive require from
    validator/puppet_server_address_validator making the
    MCollective::Util::PuppetServerAddressValidation class availablei,m instead we
    need to explicitly require it.
    ollective::Util::PuppetServerAddressValidation

--- util/puppet_agent_mgr/common.rb
+++ util/puppet_agent_mgr/common.rb
@@ -1,3 +1,5 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'util',
+                         'puppet_server_address_validation'))
 module MCollective
   module Util
     module PuppetAgentMgr
